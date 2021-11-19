eeae8c02330b90ca0cac51e93d6a35044a509ffe
Cristina <deliaadvincola@gmail.com>
2021-11-19 11:25:07 -0500

    TablaDatosPer

--- src/components/TablaDatosPer.jsx
+++ src/components/TablaDatosPer.jsx
@@ -5,32 +5,48 @@ import axios from "axios";
 const TablaDatosPer = () => {
   return (
     <>
-      <div className="flex-col items-center justify-center">
+      <div className=" flex flex-col items-center justify-center">
         <section className="flex items-center justify-center w-full h-screen">
-          <div className="flex flex-wrap items-center justify-around w-5/12 h-2/4 bg-plomoIntermedio p-10 text-white">
-            <label>Nombres</label>
-            <input type="text" name="" id="" />
+          <div className="flex flex-wrap items-center justify-around w-5/12 bg-plomoIntermedio p-10 text-white">
+            <div className="flex flex-row items-center justify-center mr-px">
+              <label>Nombres</label>
+              <input type="text" name="" id="" />
+            </div>
 
-            <label>Apellidos</label>
-            <input type="text" name="" id="" />
+            <div className="flex flex-row items-center justify-center mr-px">
+              <label>Apellidos</label>
+              <input type="text" name="" id="" />
+            </div>
 
-            <label>DNI</label>
-            <input type="text" name="" id="" />
+            <div>
+              <label>DNI</label>
+              <input type="text" name="" id="" />
+            </div>
 
-            <label>Teléfono</label>
-            <input type="text" name="" id="" />
+            <div>
+              <label>Teléfono</label>
+              <input type="text" name="" id="" />
+            </div>
 
-            <label>Correo</label>
-            <input type="text" name="" id="" />
+            <div>
+              <label>Correo</label>
+              <input type="text" name="" id="" />
+            </div>
 
-            <label>Carrera</label>
-            <input type="text" name="" id="" />
+            <div>
+              <label>Carrera</label>
+              <input type="text" name="" id="" />
+            </div>
 
-            <label>Area</label>
-            <input type="text" name="" id="" />
+            <div>
+              <label>Area</label>
+              <input type="text" name="" id="" />
+            </div>
 
-            <label>Turno</label>
-            <input type="text" name="" id="" />
+            <div>
+              <label>Turno</label>
+              <input type="text" name="" id="" />
+            </div>
           </div>
         </section>
       </div>
