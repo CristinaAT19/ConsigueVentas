2054ec0fff7e40ee23f018242a0eff6629cca320
Jairo Espinoza <50997004+jairo1397@users.noreply.github.com>
2021-11-26 13:13:00 -0500

    logo, iconos de tabla

--- src/partials/Sidebar.js
+++ src/partials/Sidebar.js
@@ -66,34 +66,17 @@ function Sidebar({
           {/* Close button */}
           <button
             ref={trigger}
-            className="lg:hidden text-gray-500 hover:text-gray-400"
+            //className="lg:hidden text-gray-500 hover:text-gray-400"
             onClick={() => setSidebarOpen(!sidebarOpen)}
             aria-controls="sidebar"
             aria-expanded={sidebarOpen}
           >
             <span className="sr-only">Close sidebar</span>
-            <svg className="w-6 h-6 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path d="M10.7 18.7l1.4-1.4L7.8 13H20v-2H7.8l4.3-4.3-1.4-1.4L4 12z" />
-            </svg>
+            
           </button>
           {/* Logo */}
           <NavLink exact to="/" className="block">
-            <svg width="32" height="32" viewBox="0 0 32 32">
-              <defs>
-                <linearGradient x1="28.538%" y1="20.229%" x2="100%" y2="108.156%" id="logo-a">
-                  <stop stopColor="#A5B4FC" stopOpacity="0" offset="0%" />
-                  <stop stopColor="#A5B4FC" offset="100%" />
-                </linearGradient>
-                <linearGradient x1="88.638%" y1="29.267%" x2="22.42%" y2="100%" id="logo-b">
-                  <stop stopColor="#38BDF8" stopOpacity="0" offset="0%" />
-                  <stop stopColor="#38BDF8" offset="100%" />
-                </linearGradient>
-              </defs>
-              <rect fill="#6366F1" width="32" height="32" rx="16" />
-              <path d="M18.277.16C26.035 1.267 32 7.938 32 16c0 8.837-7.163 16-16 16a15.937 15.937 0 01-10.426-3.863L18.277.161z" fill="#4F46E5" />
-              <path d="M7.404 2.503l18.339 26.19A15.93 15.93 0 0116 32C7.163 32 0 24.837 0 16 0 10.327 2.952 5.344 7.404 2.503z" fill="url(#logo-a)" />
-              <path d="M2.223 24.14L29.777 7.86A15.926 15.926 0 0132 16c0 8.837-7.163 16-16 16-5.864 0-10.991-3.154-13.777-7.86z" fill="url(#logo-b)" />
-            </svg>
+            <img className=" w-8 h-8" src="https://desarrollo.consigueventas.com/Frontend/Recursos/logoCompleto.png" alt="" />
           </NavLink>
         </div>
 
@@ -133,7 +116,7 @@ function Sidebar({
                           </div>
                           {/* Icon */}
                           <div className="flex flex-shrink-0 ml-2">
-                            <svg className={`w-3 h-3 flex-shrink-0 ml-1 fill-current text-gray-400 ${open && 'transform rotate-180'}`} viewBox="0 0 12 12">
+                            <svg className={`w-3 h-3 flex-shrink-0 ml-1 fill-current text-gray-50 ${open && 'transform rotate-180'}`} viewBox="0 0 12 12">
                               <path d="M5.9 11.4L.5 6l1.4-1.4 4 4 4-4L11.3 6z" />
                             </svg>
                           </div>
@@ -187,7 +170,7 @@ function Sidebar({
                           </div>
                           {/* Icon */}
                           <div className="flex flex-shrink-0 ml-2">
-                            <svg className={`w-3 h-3 flex-shrink-0 ml-1 fill-current text-gray-400 ${open && 'transform rotate-180'}`} viewBox="0 0 12 12">
+                            <svg className={`w-3 h-3 flex-shrink-0 ml-1 fill-current text-gray-50 ${open && 'transform rotate-180'}`} viewBox="0 0 12 12">
                               <path d="M5.9 11.4L.5 6l1.4-1.4 4 4 4-4L11.3 6z" />
                             </svg>
                           </div>
@@ -232,8 +215,8 @@ function Sidebar({
             <button onClick={() => setSidebarExpanded(!sidebarExpanded)}>
               <span className="sr-only">Expand / collapse sidebar</span>
               <svg className="w-6 h-6 fill-current sidebar-expanded:rotate-180" viewBox="0 0 24 24">
-                <path className="text-gray-400" d="M19.586 11l-5-5L16 4.586 23.414 12 16 19.414 14.586 18l5-5H7v-2z" />
-                <path className="text-gray-600" d="M3 23H1V1h2z" />
+                <path className="text-gray-50" d="M19.586 11l-5-5L16 4.586 23.414 12 16 19.414 14.586 18l5-5H7v-2z" />
+                <path className="text-gray-50" d="M3 23H1V1h2z" />
               </svg>
             </button>
           </div>
