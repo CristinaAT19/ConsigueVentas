a0952697db15554baa24779d83fd2c5cbfb8ec64
jiancarlos <jiancarlosmarcosp@gmail.com>
2021-12-06 13:04:10 -0500

    data personal

--- src/components/TablaDatosPer.jsx
+++ src/components/TablaDatosPer.jsx
@@ -14,11 +14,10 @@ const TablaDatosPer = () => {
   <div className="text-blueGray-400 text-2xl font-bold mt-4 uppercase text-center">
     <h2 className="py-4">Datos Personales</h2>
   </div>
-  <section>
-    <div className="w-full lg:w-8/12 px-4 mx-auto mt-6">         
-      <div className="px-4 lg:px-10 py-10 pt-0">
-        <form className=" p-4 border-2 border-yellow-500">            
-            <div className="flex flex-wrap justify-center mt-28">
+  <section clasName="">
+    <div className="w-4/5 mx-auto">
+        <form className="border py-4 border-yellow-500 shadow-md bg-indigo-100">            
+            <div className="flex flex-wrap justify-center">
               <div className="px-4">
                 <div className="relative w-full mb-3">
                   <label className="block uppercase text-blueGray-600 text-xs font-bold mb-2">
@@ -67,10 +66,8 @@ const TablaDatosPer = () => {
                   <input type="email" className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow-md focus:ring-yellow-500" value={user['turno']}/>
                 </div>
               </div>
-              
             </div>
         </form>
-      </div>
     </div>
   </section>
 </>
