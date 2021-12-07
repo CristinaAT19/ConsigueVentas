ca7ac02cedc28c790b90d4fb9f9cf188a0809973
HuamanAngel <oxipusioxx12@gmail.com>
2021-12-07 09:56:57 -0500

    Agregado lista administradores y corregido perfil de usuario

--- src/partials/Sidebar.js
+++ src/partials/Sidebar.js
@@ -277,7 +277,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                           <li className="mb-1 last:mb-0">
                             <NavLink
                               exact
-                              to="/"
+                              to="/listaAdministradores"
                               className="block text-gray-50 hover:text-gray-600 transition duration-150 truncate"
                             >
                               <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
