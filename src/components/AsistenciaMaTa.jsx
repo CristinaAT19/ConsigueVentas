835594db03e4948dd2217a5e678b6e9aaa8775f1
Jairo Espinoza <50997004+jairo1397@users.noreply.github.com>
2021-12-09 11:28:37 -0500

    dashboard turno mañana y tarde

---
+++ src/components/AsistenciaMaTa.jsx
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from 'react'
+import axios from "axios";
+// import { Pie } from 'react-chartjs-2';
+import { setToken, getToken, removeToken } from "../dist/Token";
+import { Doughnut } from 'react-chartjs-2';
+import CerrarSesion from './CerrarSesion';
+import { distSetAutentication } from '../dist/Autentication';
+
+
+
+const AsistenciaMaTa = () => {
+  /*mañana*/
+  const [puntualidad, setPuntualidad] = useState([]);
+  const [v_puntualidad, setV_Puntualidad] = useState([]);
+  const [tardanza, setTardanza] = useState([]);
+  const [v_tardanza, setV_Tardanza] = useState([]);
+  const [faltas_in, setFaltasIn] = useState([]);
+  const [v_faltas_in, setV_Faltas_in] = useState([]);
+  const [faltas_jus, setFaltasJus] = useState([]);
+  const [v_faltas_jus, setV_Faltas_jus] = useState([]);
+  const [sin_marcar, setSin_mar] = useState([]);
+  const [v_sin_marcar, setV_sin_mar] = useState([]);
+
+
+
+
+  const dataManana = {
+    labels: [puntualidad, tardanza, faltas_in, faltas_jus, sin_marcar],
+    datasets: [{
+      backgroundColor: ['green', 'yellow', 'red', 'blue', 'gray'],
+      hoverBackgroundColor: 'rgba(255,0,0,0.2)',
+      data: [v_puntualidad, v_tardanza, v_faltas_in, v_faltas_jus, v_sin_marcar]
+    }]
+  };
+
+  const opciones = {
+    maintainAspectRatio: false,
+    responsive: true
+  }
+
+  const peticionApiAsistenciaManana = async () => {
+    await axios.get(`${process.env.REACT_APP_API_URL}/api/dashboard_ma_ta`,
+      {
+        headers: {
+
+          Authorization: `Bearer ${getToken()}`
+
+        }
+      })
+      .then(response => {
+        setPuntualidad(response.data.puntualidad);
+        setV_Puntualidad(response.data.v_puntualidad);
+        setTardanza(response.data.tardanza);
+        setV_Tardanza(response.data.v_tardanza);
+        setFaltasIn(response.data.faltas_in);
+        setV_Faltas_in(response.data.v_faltas_in);
+        setFaltasJus(response.data.faltas_jus);
+        setV_Faltas_jus(response.data.v_faltas_jus);
+        setSin_mar(response.data.sin_marcar);
+        setV_sin_mar(response.data.v_sin_marcar);
+      })
+      .catch((e) => {
+        if(e.response.status === 403){
+          console.log("No tienes permisos para ver esta información");
+        }
+
+        if(e.response.status === 401){
+          console.log("El token expiro o no te has aunteticado");
+          
+          distSetAutentication(false);
+          removeToken();
+
+        }
+      });
+
+  }
+  useEffect(() => {
+    peticionApiAsistenciaManana();
+  }, [])
+return (
+
+    <Doughnut data={dataManana} options={opciones} />
+  
+    )}
+
+export default AsistenciaMaTa;
--- src/pages/VistasAdmin/Dashboard.jsx
+++ src/pages/VistasAdmin/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import AsistenciaMa from "../../components/AsistenciaMa";
+import AsistenciaMaTa from "../../components/AsistenciaMaTa";
 import AsistenciaPer from "../../components/AsistenciaPer";
 import AsistenciaTarde from "../../components/AsistenciaTarde";
 import { UserContext } from "../../components/context/UserContext";
@@ -7,46 +8,15 @@ import { UserContext } from "../../components/context/UserContext";
 import TablaDia from "../../components/TablaDia";
 import TablaSin from "../../components/TablaSin";
 const Dashboard = () => {
-
   const { user } = useContext(UserContext);
 
   return (
     <>
-      <h1 className="text-center text-3xl">Dashboard</h1>
-      <div>
-        {user['id_TipoUsuario'] == 1 ? (
-
-          <div className="my-14 w-full flex justify-evenly mb-4" style={{ height: "400px" }}>
-
-            <div className="bg-white border-transparent rounded-lg shadow-xl flex flex-col justify-center " style={{ width: "40%" }} >
-              <div className="bg-gradient-to-b from-gray-300 to-gray-100 uppercase text-gray-800 border-b-2 border-gray-300 rounded-tl-lg rounded-tr-lg p-1">
-                <h1 className="font-bold uppercase text-gray-600 w-full text-center text-xl">
-                  Asistencia de Dia (Turno Mañana)
-                </h1>
-              </div>
-              <div className="h-full py-4">
-                <AsistenciaMa />
-              </div>
-            </div>
-
-
-            <div className="bg-white border-transparent rounded-lg shadow-xl flex flex-col justify-center " style={{ width: "40%" }} >
-              <div className="bg-gradient-to-b from-gray-300 to-gray-100 uppercase text-gray-800 border-b-2 border-gray-300 rounded-tl-lg rounded-tr-lg p-1">
-                <h1 className="font-bold uppercase text-gray-600 w-full text-center text-xl">
-                  Asistencia de Dia (Turno Tarde)
-                </h1>
-              </div>
-              <div className="h-full py-4">
-                <AsistenciaTarde />
-              </div>
-            </div>
-
-
-          </div>
-        ) : null
-        }
-        <div className="my-14 w-full flex justify-evenly mb-4" style={{ height: "400px" }} >
-          <div className="bg-white border-transparent rounded-lg shadow-xl flex flex-col justify-center" style={{ width: "40%" }} >
+    {/* Dahsboard personal */}
+      <div className="my-4">
+        <h1 className=" mt-2 text-center text-3xl">Dashboard personal</h1>
+        <div  className="border-gray-500 w-full flex justify-evenly my-2 " style={{ height: "700px" }} >
+          <div  className="bg-white border-transparent rounded-lg shadow-xl flex flex-col justify-center" style={{ width: "80%" }}  >
             <div className="bg-gradient-to-b from-gray-300 to-gray-100 uppercase text-gray-800 border-b-2 border-gray-300 rounded-tl-lg rounded-tr-lg p-1">
               <h1 className="font-bold uppercase text-gray-600 w-full text-center text-xl">
                 Mi asistencia del mes
@@ -58,31 +28,78 @@ const Dashboard = () => {
           </div>
         </div>
       </div>
-      {user['id_TipoUsuario'] == 1 ? (
 
+
+      {/* Dashboards de administrador */}
+      {user["id_TipoUsuario"] == 1 ? (
+      <div className="my-4">
+        <h1 className="text-center text-3xl">Dashboard de Administrador</h1>
+          <div>
+            <div className="mt-2 w-full flex justify-evenly mb-4" style={{ height: "400px" }} >
+              <div className="bg-white border-transparent rounded-lg shadow-xl flex flex-col justify-center " style={{ width: "40%" }} >
+                <div className="bg-gradient-to-b from-gray-300 to-gray-100 uppercase text-gray-800 border-b-2 border-gray-300 rounded-tl-lg rounded-tr-lg p-1">
+                  <h1 className="font-bold uppercase text-gray-600 w-full text-center text-xl">
+                    Asistencia del Dia (Turno Mañana)
+                  </h1>
+                </div>
+                <div className="h-full py-4">
+                  <AsistenciaMa />
+                </div>
+              </div>
+
+              <div className="bg-white border-transparent rounded-lg shadow-xl flex flex-col justify-center " style={{ width: "40%" }} >
+                <div className="bg-gradient-to-b from-gray-300 to-gray-100 uppercase text-gray-800 border-b-2 border-gray-300 rounded-tl-lg rounded-tr-lg p-1">
+                  <h1 className="font-bold uppercase text-gray-600 w-full text-center text-xl">
+                    Asistencia del Dia (Turno Tarde)
+                  </h1>
+                </div>
+                <div className="h-full py-4">
+                  <AsistenciaTarde />
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+          <div className="my-14 w-full flex justify-evenly mb-4" style={{ height: "400px" }} >
+            <div className="bg-white border-transparent rounded-lg shadow-xl flex flex-col justify-center"  style={{ width: "40%" }} >
+              <div className="bg-gradient-to-b from-gray-300 to-gray-100 uppercase text-gray-800 border-b-2 border-gray-300 rounded-tl-lg rounded-tr-lg p-1">
+                <h1 className="font-bold uppercase text-gray-600 w-full text-center text-xl">
+                  Asistencia del Dia (Mañana y Tarde)
+                </h1>
+              </div>
+              <div className="h-full py-4">
+                <AsistenciaMaTa />
+              </div>
+            </div>
+          </div>
+        
+      </div>
+      ) : null}
+
+
+
+
+      {/* Tablas de administrador */}
+      {user["id_TipoUsuario"] == 1 ? (
         <div className="bg-white m-20 rounded-t-3xl text-center">
           <h1 className="border-b-2 text-3xl">Tabla de Asistencia </h1>
-          <div className="my-0 mx-auto py-4" style={{ width: "97%" }} >
+          <div className="my-0 mx-auto py-4" style={{ width: "97%" }}>
             <TablaDia />
           </div>
         </div>
-      ) : null
-      }
+      ) : null}
 
-      {user['id_TipoUsuario'] == 1 ? (
-
+      {user["id_TipoUsuario"] == 1 ? (
         <div className="bg-white m-20 rounded-t-3xl text-center">
           <h1 className="border-b-2 text-3xl">Vista de empleados sin marcar</h1>
-          <div className="my-0 mx-auto py-4" style={{ width: "97%" }} >
+          <div className="my-0 mx-auto py-4" style={{ width: "97%" }}>
             <TablaSin />
           </div>
         </div>
-      ) : null
-      }
+      ) : null}
     </>
   );
 };
 
-
 export default Dashboard;
-
