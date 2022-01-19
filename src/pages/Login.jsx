3a20abcb5e6702172a10c0afa3e57b7874328871
HuamanAngel <oxipusioxx12@gmail.com>
2022-01-19 11:03:26 -0500

    .

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -123,7 +123,7 @@ const Login = () => {
               id="boton"
               onClick={()=>{
 
-                window.location.replace("https://erp.consigueventas.com/sistema/marcacion/");
+                window.location.replace("https://erp.consigueventas.com/marcacion/");
               }}  
               className="cursor-pointer py-2 px-6 block duration-75 bg-naranja hover:bg-yellow-300 text-white font-bold w-32 text-center rounded">
               Marcación
@@ -199,7 +199,7 @@ const Login = () => {
               id="boton"
               onClick={()=>{
 
-                window.location.replace("https://erp.consigueventas.com/sistema/marcacion/");
+                window.location.replace("https://erp.consigueventas.com/marcacion/");
               }}  
               className="cursor-pointer mt-3 py-2 px-6  duration-75 bg-naranja hover:bg-yellow-300 text-white font-bold w-full text-center rounded block md:hidden">
               Marcación
